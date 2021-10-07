55ebd74a01b8c8be4a22e2287203b5c62e6bd9fa
Mazurenko Sergei <comandor1234@gmail.com>
2021-10-07 23:23:31 +0300

    First commit for init

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
+using System;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] int weaponDamage = 1;
+
+        Health target;
+        float timeSinceLastAttack = Mathf.Infinity;
+
+        private void Update()
+        {
+            timeSinceLastAttack += Time.deltaTime;
+
+            if (target == null) return;
+            if (target.IsDead()) return;
+
+            if (!GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.transform.position);
+
+            }
+            else 
+            {
+                GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+                
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            transform.LookAt(target.transform);
+            if (timeSinceLastAttack >= timeBetweenAttacks)
+            {
+                // This will trigger the Hit() event.
+                TriggerAttack();
+                timeSinceLastAttack = 0;
+
+            }
+        }
+
+
+
+        private void TriggerAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("stopAttack");
+            GetComponent<Animator>().SetTrigger("attack");
+        }
+
+        // Animation Event
+        void Hit()
+        {
+            if(target == null) { return; }
+                target.TakeDamage(weaponDamage);
+                target.Death();
+        }
+
+        public bool CanAttack(GameObject combatTarget)
+        {
+            if (combatTarget == null) { return false; }
+            Health targetToTest = combatTarget.GetComponent<Health>();
+            return targetToTest != null && !targetToTest.IsDead();
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+        }
+
+        public void Attack(GameObject combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.GetComponent<Health>();
+        }
+
+        public void Cancel()
+        {
+            StopAttack();
+            target = null;
+        }
+
+        private void StopAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("attack");
+            GetComponent<Animator>().SetTrigger("stopAttack");
+        }
+    }
+}
